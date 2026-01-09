eeb9c12a7ad26cfe7b805dbf8c4eafbb89ad2601
rishaba2428 <rishabamanickamoorthi24@gmail.com>
2026-01-09 09:31:25 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,16 @@
+public class Task {
+    private String taskdescription;
+
+    public Task(String taskdescription)
+    {
+        this.taskdescription=taskdescription;
+    }
+
+    public String getTaskdescription() {
+        return taskdescription;
+    }
+
+    public void setTaskdescription(String taskdescription) {
+        this.taskdescription = taskdescription;
+    }
+}
